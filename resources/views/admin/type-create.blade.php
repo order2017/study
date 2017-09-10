bb8a95549cddb0d71e0b56355caa7b0e2a5d40aa
陈小龙 <13480731740@qq.com>
2017-09-10 08:34:10 +0800

    优化样式，添加弹层，优化逻辑

--- resources/views/admin/type-create.blade.php
+++ resources/views/admin/type-create.blade.php
@@ -19,7 +19,13 @@
 
     <caption>
         <a href="/admin/type" type="button" class="btn btn-default">分类列表</a>
-        <a href="/admin/type-create" type="button" class="btn btn-success">添加分类</a>
+        @if(count(explode(",",Request::get('path')))=="2")
+            <a href="{{ Request::getRequestUri() }}" type="button" class="btn btn-success">添加二级分类</a>
+        @elseif(count(explode(",",Request::get('path')))=="3")
+            <a href="{{ Request::getRequestUri() }}" type="button" class="btn btn-success">添加三级分类</a>
+        @else
+            <a href="/admin/type-create" type="button" class="btn btn-success">添加一级分类</a>
+        @endif
     </caption>
 
     <tr>
@@ -73,7 +79,7 @@
                 </div>
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-primary">登录</button>
+                        <button type="submit" class="btn btn-primary">确定</button>
                         <button type="reset" class="btn btn-danger">重置</button>
                     </div>
                 </div>
--- resources/views/admin/type.blade.php
+++ resources/views/admin/type.blade.php
@@ -5,9 +5,14 @@
     <title>无限级分类管理</title>
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+    <script src="../layer/layer.js"></script>
     <style type="text/css">
         html,body{
             margin:10px 50px 10px 50px;
+        }
+        tr,th{
+            text-align: center;
+            vertical-align: middle;
         }
     </style>
 </head>
@@ -17,13 +22,14 @@
 
     <caption>
         <a href="/admin/type" type="button" class="btn btn-default">分类列表</a>
-        <a href="/admin/type-create" type="button" class="btn btn-success">添加分类</a>
+        <a href="/admin/type-create" type="button" class="btn btn-success">添加一级分类</a>
     </caption>
 
     <thead>
     <tr>
         <th>ID</th>
         <th>分类名称</th>
+        <th>所属上级</th>
         <th>标题</th>
         <th>关键字</th>
         <th>描述</th>
@@ -37,49 +43,52 @@
         <tr>
             <td>{{ $list->id }}</td>
             <td>{{ $list->name }}</td>
+            <td>顶层</td>
             <td>{{ $list->title }}</td>
             <td>{{ $list->keywords }}</td>
             <td>{{ $list->description }}</td>
-            <td><a href="/admin/type-create?pid={{ $list->id }}&path={{ $list->path }}{{ $list->id }}">添加子类</a></td>
+            <td><a href="/admin/type-create?pid={{ $list->id }}&path={{ $list->path }}{{ $list->id }}">添加二级子类</a></td>
             <td>
                 @if($list->is_lou)
-                    <button type="button" class="btn btn-success">是</button>
+                    是
                 @else
-                    <button type="button" class="btn btn-danger">否</button>
+                    否
                 @endif
             </td>
             <td>修改 | <a href="javascript:void(0);" onclick="del({{ $list->id }})">删除</a></td>
         </tr>
         @foreach($list->parent as $line)
-            <tr bgcolor="#ffc0cb">
+            <tr>
                 <td>{{ $line->id }}</td>
                 <td>{{ $line->name }}</td>
+                <td>{{ $list->name }}</td>
                 <td>{{ $line->title }}</td>
                 <td>{{ $line->keywords }}</td>
                 <td>{{ $line->description }}</td>
-                <td><a href="/admin/type-create?pid={{ $line->id }}&path={{ $line->path }}{{ $line->id }}">添加子类</a></td>
+                <td><a href="/admin/type-create?pid={{ $line->id }}&path={{ $line->path }}{{ $line->id }}">添加三级子类</a></td>
                 <td>
                     @if($line->is_lou)
-                        <button type="button" class="btn btn-success">是</button>
+                        是
                     @else
-                        <button type="button" class="btn btn-danger">否</button>
+                        否
                     @endif
                 </td>
                 <td>修改 | <a href="javascript:void(0);" onclick="del({{ $line->id }})">删除</a></td>
             </tr>
             @foreach($line->parent as $san)
-                <tr bgcolor="#d3d3d3">
+                <tr>
                     <td>{{ $san->id }}</td>
                     <td>{{ $san->name }}</td>
+                    <td>{{ $line->name }}</td>
                     <td>{{ $san->title }}</td>
                     <td>{{ $san->keywords }}</td>
                     <td>{{ $san->description }}</td>
                     <td></td>
                     <td>
                         @if($san->is_lou)
-                            <button type="button" class="btn btn-success">是</button>
+                            是
                         @else
-                            <button type="button" class="btn btn-danger">否</button>
+                            否
                         @endif
                     </td>
                     <td>修改 | <a href="javascript:void(0);" onclick="del({{ $san->id }})">删除</a></td>
@@ -94,13 +103,20 @@
 <script type="text/javascript">
 
     function del(id) {
-        if (confirm("您确定要删除吗？")){
-            $.post('/admin/type-delete/'+id,{'_token':'{{ csrf_token() }}','_method':'get'},function (data) {
-                if (data==1){
-                    window.location.reload();
-                }
-            });
-        }
+
+        layer.msg('您确定要删除吗？', {
+            time: 0 //不自动关闭
+            ,btn: ['删除', '取消']
+            ,yes: function(index){
+                layer.close(index);
+                $.post('/admin/type-delete/'+id,{'_token':'{{ csrf_token() }}','_method':'get'},function (data) {
+                    if (data==1){
+                        window.location.reload();
+                    }
+                });
+            }
+        });
+
     }
 
 </script>
